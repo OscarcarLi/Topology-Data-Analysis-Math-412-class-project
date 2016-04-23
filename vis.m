c2a9fb38320334608935efe55794d7cc560ba06d
z3t1 <zhfyyq@gmail.com>
2016-04-22 21:17:38 -0400

    Vis script

--- vis.m
+++ vis.m
@@ -1,4 +1,11 @@
-point_cloud = csvread('n30.csv',1,2);
-tmp = importdata('n30.csv');
-names = tmp.textdata(2:end,2);
+ point_cloud = csvread('anivec.csv',1,2);
+ tmp = importdata('anivec.csv');
+ names = tmp.textdata(2:end,2);
 
+no_dims = 2;
+initial_dims = 20;
+perplexity = 30;
+
+mappedX = tsne(point_cloud, [], no_dims, initial_dims, perplexity);
+
+gscatter(mappedX(:,1), mappedX(:,2), names);
